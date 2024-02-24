043a28fe94a7a80de67f64c667c0defbc40b01dc
bhuvii07 <135493045+bhuvii07@users.noreply.github.com>
2024-02-24 11:19:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+
+console.log("Welcome to Spotify")
+
+//Initialize the variables
+let songIndex = 0;
+let audioElement = new Audio('-1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+
+let songs = [
+    {songName: "Let-me-love", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Let-me-love", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Let-me-love", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Let-me-love", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Let-me-love", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Let-me-love", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Let-me-love", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Let-me-love", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Let-me-love", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Let-me-love", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+]
+
+
+
+
+// audioElement.play();
+
+//Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
+        gif.style.opacity = 0;
+    }
+})
+// listen to event
+audioElement.addEventListener('timeupdate', ()=>{
+    
+// Update Seekbar
+   progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+   
+   myProgressBar.value = progress;
+})
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
